57321d023333af6e8668b09b7493655941824faf
Jortega23 <jortegs23@gmail.com>
2020-02-13 21:02:18 -0800

    added javascript to form
    Javascript added

---
+++ form.js
@@ -0,0 +1,118 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+//Show for success
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check email is valid
+function checkEmail(input){
+    if(re.test(input.value.trim())) {
+        showSuccess(input)
+    }else{
+        showError(input, 'Email is not valid')
+    }
+}
+
+//Event Listeners easiest way with if/else statements
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+    
+//     if(username.value === ''){
+//         showError(username,'Username is required')
+//     } else {
+//         showSuccess(username)
+//     }
+//     if(email.value === ''){
+//         showError(email,'Email is required')
+//     }
+//     else if(!checkEmailValid(email.value)){
+//         showError(email,'Email is not valid')
+//     } else {
+//         showSuccess(email)
+//     }
+//     if(password.value === ''){
+//         showError(password,'Password is required')
+//     } else {
+//         showSuccess(password)
+//     }
+//     if(password2.value === ''){
+//         showError(password2,'Passwords need to match')
+//     } else {
+//         showSuccess(password2)
+//     }
+// })
+
+
+//event listeners refactored----------------------------------
+//check fields 
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() == ''){
+            showError(input, `${getFieldName(input)} is required`)
+        }else{
+            showSuccess(input)
+        }
+    })
+}
+
+//check input length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} needs to be at least ${min} characters`)
+    }else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max}`)
+    }
+    else{
+        showSuccess(input)
+    }
+}
+
+//check password length
+function checkPassword(input, min, max){
+    if(input.value.length < min){
+        showError(input, 'It needs to be longer')
+    }else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max}`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+//check password match
+function passwordMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, "Passwords do not match")
+    }else{
+        showSuccess(input1,input2)
+    }
+}
+
+
+//check field name
+ function getFieldName(input){
+     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+ }
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15);
+    checkPassword(password, 6, 25);
+    checkEmail(email)
+    passwordMatch(password, password2)
+})
